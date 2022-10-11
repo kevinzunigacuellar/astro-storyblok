f00cbdb3a7911426dfe53d382677c7d19d2131fa
kevinzunigacuellar <46791833+kevinzunigacuellar@users.noreply.github.com>
2022-10-10 21:40:25 -0400

    add styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import storyblok from '@storyblok/astro';
 import tailwind from '@astrojs/tailwind';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
   integrations: [
     storyblok({
-      accessToken: '9aAbQxfwe7F9SH5LvZXTNQtt', // prodkey: Th4BbSEt49KDfg5mFIsylwtt
+      accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
       components: {
         page: 'storyblok/Page',
         blogpost: 'storyblok/BlogPost',
-        preview_post: 'storyblok/Post',
+        preview_post: 'storyblok/Posts',
       },
       apiOptions: {
         region: 'us',
