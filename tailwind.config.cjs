d024e867634bafeb4a82cd1a69430de1d87e44eb
kevinzunigacuellar <46791833+kevinzunigacuellar@users.noreply.github.com>
2022-10-24 13:03:11 -0400

    rfc pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-  theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-    },
-    extend: {
-      colors: {
-        primary: '#50b0ae',
-        secondary: '#1c243d',
-      },
-    },
-  },
+  theme: {},
   plugins: [require('@tailwindcss/typography')],
 };
