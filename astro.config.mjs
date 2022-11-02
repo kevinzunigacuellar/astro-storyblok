01a67fed549ead4c6bbdfb8cb43cd97cfb658111
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-11-02 17:49:33 -0400

    add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,22 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import tailwind from '@astrojs/tailwind';
-import { loadEnv } from 'vite';
+import vercel from '@astrojs/vercel/serverless'
 
-const env = loadEnv(import.meta.env.MODE, process.cwd(), 'STORYBLOK');
-
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken:
-        import.meta.env.MODE === 'development'
-          ? env.STORYBLOK_PREVIEW_TOKEN
-          : env.STORYBLOK_PUBLIC_TOKEN,
-      components: {
-        page: 'storyblok/Page',
-        blogpost: 'storyblok/BlogPost',
-        preview_post: 'storyblok/Posts',
-        about_section: 'storyblok/AboutSection',
-      },
-      apiOptions: {
-        region: 'us',
-      },
-    }),
-    tailwind(),
-  ],
+  integrations: [storyblok({
+    accessToken: 'Th4BbSEt49KDfg5mFIsylwtt',
+    components: {
+      page: 'storyblok/Page',
+      blogpost: 'storyblok/BlogPost',
+      preview_post: 'storyblok/Posts',
+      about_section: 'storyblok/AboutSection'
+    },
+    apiOptions: {
+      region: 'us'
+    }
+  }), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
